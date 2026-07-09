a1534d528bd1357053684a47e7f31cfeac3b817e
Manav Patel <manavpatel3338@gmail.com>
2026-07-09 11:55:49 +0530

    Added root route

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import contactRoute from "./routes/contactRoute.js";
 
 dotenv.config();
@@ -11,10 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Root Route
+app.get("/", (req, res) => {
+  res.send("🚀 Portfolio Backend is Running Successfully");
+});
+
 app.use("/api/contact", contactRoute);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server Running on ${PORT}`);
+  console.log(`Server Running on ${PORT}`);
 });
