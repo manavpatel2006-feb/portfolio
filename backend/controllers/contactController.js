acc6a04a8fa0ba9dc7e05e7be926ee28eceea84a
Manav Patel <manavpatel3338@gmail.com>
2026-07-09 13:11:42 +0530

    Switched to Resend

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -6,17 +6,18 @@ export const sendMail = async (req, res) => {
   const { name, email, mobile, subject, message } = req.body;
 
   try {
-    // Admin Email
+    // Email to You
     await resend.emails.send({
       from: "Portfolio <onboarding@resend.dev>",
       to: "manav6738@gmail.com",
       subject: `📩 ${subject}`,
       html: `
-        <h2>New Contact</h2>
+        <h2>New Portfolio Contact</h2>
 
         <p><b>Name:</b> ${name}</p>
         <p><b>Email:</b> ${email}</p>
         <p><b>Mobile:</b> ${mobile}</p>
+        <p><b>Subject:</b> ${subject}</p>
         <p><b>Message:</b> ${message}</p>
       `,
     });
@@ -25,17 +26,21 @@ export const sendMail = async (req, res) => {
     await resend.emails.send({
       from: "Portfolio <onboarding@resend.dev>",
       to: email,
-      subject: "Thank You",
+      subject: "Thank You For Contacting Me",
       html: `
-        <h2>Hello ${name}</h2>
+        <h2>Hello ${name} 👋</h2>
 
         <p>Thank you for contacting me.</p>
+
+        <p>Your message has been received successfully.</p>
 
         <p>I will reply as soon as possible.</p>
 
         <br>
 
-        <b>Manav Patel</b>
+        <h3>Regards,</h3>
+
+        <h3>Manav Patel</h3>
       `,
     });
 
@@ -45,7 +50,7 @@ export const sendMail = async (req, res) => {
     });
 
   } catch (err) {
-    console.log(err);
+    console.error(err);
 
     res.status(500).json({
       success: false,
