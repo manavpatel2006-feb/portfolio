b876adc213f38cf3e7102172d8910be16455b30a
Manav Patel <manavpatel3338@gmail.com>
2026-07-09 13:09:17 +0530

    Switched to Resend

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,72 +1,41 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async (req, res) => {
   const { name, email, mobile, subject, message } = req.body;
 
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-   // await transporter.verify();//
-    console.log("✅ SMTP Connected Successfully");
-
-    // ==========================
-    // 1. EMAIL TO YOU
-    // ==========================
-console.log("Request Received");
-console.log(req.body);
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
-
-      subject: `📩 Portfolio Contact - ${subject}`,
-
+    // Admin Email
+    await resend.emails.send({
+      from: "Portfolio <onboarding@resend.dev>",
+      to: "manav6738@gmail.com",
+      subject: `📩 ${subject}`,
       html: `
-        <h2>New Contact Form</h2>
+        <h2>New Contact</h2>
 
         <p><b>Name:</b> ${name}</p>
-
         <p><b>Email:</b> ${email}</p>
-
         <p><b>Mobile:</b> ${mobile}</p>
-
-        <p><b>Subject:</b> ${subject}</p>
-
-        <p><b>Message:</b></p>
-
-        <p>${message}</p>
+        <p><b>Message:</b> ${message}</p>
       `,
     });
 
-    // ==========================
-    // 2. THANK YOU EMAIL TO USER
-    // ==========================
-
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+    // Thank You Email
+    await resend.emails.send({
+      from: "Portfolio <onboarding@resend.dev>",
       to: email,
-
-      subject: "Thank You For Contacting Me",
-
+      subject: "Thank You",
       html: `
-        <h2>Hello ${name},</h2>
+        <h2>Hello ${name}</h2>
 
         <p>Thank you for contacting me.</p>
 
-        <p>Your message has been received successfully.</p>
-
-        <p>I will reply to you as soon as possible.</p>
+        <p>I will reply as soon as possible.</p>
 
         <br>
 
-        <h3>Regards,</h3>
-
-        <h3>Manav Patel</h3>
+        <b>Manav Patel</b>
       `,
     });
 
@@ -76,7 +45,7 @@ console.log(req.body);
     });
 
   } catch (err) {
-    console.error(err);
+    console.log(err);
 
     res.status(500).json({
       success: false,
